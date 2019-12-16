53ab77584103bcb0e51938d67273b0a3bc15025a
Tony Smith <adavidsmith5@gmail.com>
2019-12-15 22:14:35 -0500

    Completed Video Application
    Used the search to get videos from you tube along with thumbnails and descriptions. Displayed those to the user and modified the styling to make it cleaner.

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = props=> {
+const VideoList = ({ videos, onVideoSelect }) => {
+    const renderedList = videos.map((video) => {
+        return <VideoItem key={ video.id.videoId } video={ video } onVideoSelect={ onVideoSelect }/>;
+    });
 
-    return <div>{ props.videos.length }</div>
+    return <div className="ui relaxed divided list">{ renderedList }</div>
 }
 
 export default VideoList;
